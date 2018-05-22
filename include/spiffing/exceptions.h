2d5299e945f6bd325bad2b141413f8a7f92774c3
Dave Cridland <dave@cridland.net>
2018-05-22 22:16:54 +0100

    Use CMake support for C++11

---
+++ include/spiffing/exceptions.h
@@ -0,0 +1,8 @@
+//
+// Created by dwd on 22/05/18.
+//
+
+#ifndef SPIFFING_EXCEPTIONS_H
+#define SPIFFING_EXCEPTIONS_H
+
+#endif //SPIFFING_EXCEPTIONS_H
---
+++ src/exceptions.cc
@@ -0,0 +1,4 @@
+//
+// Created by dwd on 22/05/18.
+//
+
